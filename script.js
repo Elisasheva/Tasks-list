ed09ee18943776b33fdd8ead430900f6368c673a
DSDi <dsdideveloper@iMacDSDi.local>
2020-09-08 12:05:01 +0200

    adding new task and showing if it's done

---
+++ script.js
@@ -0,0 +1,48 @@
+{
+    const tasks = [
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask  = (newTask) => {
+        tasks.push({
+            content: newTask,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = document.querySelector(".js-newTask").value.trim();
+        
+        if(newTask === "") {
+            return;
+        }
+
+        addNewTask(newTask);
+    }
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
